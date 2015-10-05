812fc15bdcd3739d5eb2591a8af96f93c79a88bb
buddhamagnet <buddhamagnet@gmail.com>
2015-10-05 11:37:33 +0100

    run go fmt

--- main.go
+++ main.go
@@ -1,166 +1,165 @@
 package main
 
 import (
-        "log"
-        "os"
-        "github.com/tarm/serial"
-        "fmt"
-        "time"
-        "net/http"
-        "github.com/gorilla/mux"
-        "github.com/joho/godotenv"
-        "encoding/json"
-        "io/ioutil"
-        "strconv"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/tarm/serial"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 type API struct {
-    Message string "json:message"
+	Message string "json:message"
 }
 
 // Connect to Arduino board by USB serial and send the color code.
 func initializeSerializer(color string) {
-        config := &serial.Config{Name: "/dev/tty.usbserial-A60080Ig", Baud: 9600}
-        s, err := serial.OpenPort(config)
-        if err != nil {
-                log.Fatal(err)
-        }
+	config := &serial.Config{Name: "/dev/tty.usbserial-A60080Ig", Baud: 9600}
+	s, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        n, err := s.Write([]byte(color))
-        if err != nil {
-                log.Fatal(err)
-        }
+	n, err := s.Write([]byte(color))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        buf := make([]byte, 128)
-        n, err = s.Read(buf)
-        if err != nil {
-                log.Fatal(err)
-        }
-        log.Printf("%q", buf[:n])
+	buf := make([]byte, 128)
+	n, err = s.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%q", buf[:n])
 }
 
 // Handles request to chnage light color.
 func handleLightColorRequest(w http.ResponseWriter, r *http.Request) {
-    urlParams := mux.Vars(r)
-    color := urlParams["color"]
-    HelloMessage := "Hello, " + color
+	urlParams := mux.Vars(r)
+	color := urlParams["color"]
+	HelloMessage := "Hello, " + color
 
-    message := API{HelloMessage}
-    output, err := json.Marshal(message)
+	message := API{HelloMessage}
+	output, err := json.Marshal(message)
 
-    if err != nil {
-        fmt.Println("Something went wrong!")
-    }
+	if err != nil {
+		fmt.Println("Something went wrong!")
+	}
 
-    fmt.Fprintf(w, string(output))
+	fmt.Fprintf(w, string(output))
 }
 
 // Sets the color of the light on the Arduino board.
 func setArduinoLightColor(lightColorCode string) {
-    fmt.Println("Done", lightColorCode)
-    initializeSerializer(lightColorCode);
+	fmt.Println("Done", lightColorCode)
+	initializeSerializer(lightColorCode)
 }
 
 // Initializes server and routes http requests.
 func handleHttpRequests() {
-    gorillaRoute := mux.NewRouter()
-    gorillaRoute.HandleFunc("/light/{color}", handleLightColorRequest)
-    http.Handle("/", gorillaRoute)
-    http.ListenAndServe(":3010", nil)
+	gorillaRoute := mux.NewRouter()
+	gorillaRoute.HandleFunc("/light/{color}", handleLightColorRequest)
+	http.Handle("/", gorillaRoute)
+	http.ListenAndServe(":3010", nil)
 }
 
 // Send request to Jenkins API and retrieves the status Json.
 func sendRequestToJenkinsAPI() string {
-    resp, err := http.Get("http://localhost:8080/api/json?pretty=true")
+	resp, err := http.Get("http://localhost:8080/api/json?pretty=true")
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer resp.Body.Close()
-    jenkinsJson, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	jenkinsJson, err := ioutil.ReadAll(resp.Body)
 
-    return string(jenkinsJson)
+	return string(jenkinsJson)
 }
 
 // Parses the Json from Jenkins and retrives the specified job's status code.
 func getJobsStatusFromJenkinsJson(jenkinsJson string) string {
 
-    type JobItem struct {
-        Name string
-        Url string
-        Color string
-    }
+	type JobItem struct {
+		Name  string
+		Url   string
+		Color string
+	}
 
-    type JenkinsApiData struct {
-        Mode string
-        NodeDescription string
-        Jobs []JobItem
-    }
+	type JenkinsApiData struct {
+		Mode            string
+		NodeDescription string
+		Jobs            []JobItem
+	}
 
-    msg := new(JenkinsApiData)
-    _ = json.Unmarshal([]byte(jenkinsJson), &msg)
+	msg := new(JenkinsApiData)
+	_ = json.Unmarshal([]byte(jenkinsJson), &msg)
 
-    monitoredJob := JobItem{}
+	monitoredJob := JobItem{}
 
-    for _, jobItem := range msg.Jobs {
-        if jobItem.Name == os.Getenv("job_name") {
-            monitoredJob = jobItem;
-            break;
-        }
-    }
+	for _, jobItem := range msg.Jobs {
+		if jobItem.Name == os.Getenv("job_name") {
+			monitoredJob = jobItem
+			break
+		}
+	}
 
-    return monitoredJob.Color
+	return monitoredJob.Color
 }
 
 // Handles the polling of requests to Jenkins.
 func getFrequentStatusFromJenkins() {
-    var pollingFrequency int64
+	var pollingFrequency int64
 
-    pollingFrequency, _ = strconv.ParseInt(os.Getenv("polling_frequency"), 10, 64)
+	pollingFrequency, _ = strconv.ParseInt(os.Getenv("polling_frequency"), 10, 64)
 
-    for {
-        time.Sleep(time.Second * time.Duration(pollingFrequency))
+	for {
+		time.Sleep(time.Second * time.Duration(pollingFrequency))
 
-        jenkinsJson := sendRequestToJenkinsAPI()
+		jenkinsJson := sendRequestToJenkinsAPI()
 
-        jobStatus := getJobsStatusFromJenkinsJson(jenkinsJson)
+		jobStatus := getJobsStatusFromJenkinsJson(jenkinsJson)
 
-        lightColorCode := getLightColorCode(jobStatus)
+		lightColorCode := getLightColorCode(jobStatus)
 
-        setArduinoLightColor(lightColorCode)
+		setArduinoLightColor(lightColorCode)
 
-        fmt.Println("Done", jobStatus)
-    }
+		fmt.Println("Done", jobStatus)
+	}
 }
 
 // Get associated light code from Jenkins job for Arduino.
-func getLightColorCode(statusColor string) string{
+func getLightColorCode(statusColor string) string {
 
-    lightColorCodes := map[string]string{
-      "blue": "1",
-      "blue_anime": "0",
-      "yellow": "2",
-      "yellow_anime": "20",
-      "red": "3",
-      "red_anime": "30",
-      "grey": "4",
-      "grey_anime": "40",
-      "aborted": "4",
-      "aborted_anime": "40",
-      "disabled": "4",
-      "disabled_anime": "40",
-    }
+	lightColorCodes := map[string]string{
+		"blue":           "1",
+		"blue_anime":     "0",
+		"yellow":         "2",
+		"yellow_anime":   "20",
+		"red":            "3",
+		"red_anime":      "30",
+		"grey":           "4",
+		"grey_anime":     "40",
+		"aborted":        "4",
+		"aborted_anime":  "40",
+		"disabled":       "4",
+		"disabled_anime": "40",
+	}
 
-    return lightColorCodes[statusColor]
+	return lightColorCodes[statusColor]
 }
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-    //handleHttpRequests()
-    getFrequentStatusFromJenkins()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	//handleHttpRequests()
+	getFrequentStatusFromJenkins()
 }
-
