1ce626e612e15cce7a56ae80659823ec64b527ca
Vin Gardner <vcrgardner@gmail.com>
2015-10-17 23:03:33 +0100

    Adds tests

--- main_test.go
+++ main_test.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func Test_GetLightColorCode(t *testing.T) {
-    lightColorCode := getLightColorCode("blue")
-     if lightColorCode != "1" {
-        t.Error("Expected", "1", "got", lightColorCode)
-    }
+	lightColorCode := getLightColorCode("blue")
+	if lightColorCode != "1" {
+		t.Error("Expected", "1", "got", lightColorCode)
+	}
 
-    lightColorCode2 := getLightColorCode("blue_anime")
-     if lightColorCode2 != "0" {
-        t.Error("Expected", "0", "got", lightColorCode2)
-    }
+	lightColorCode2 := getLightColorCode("blue_anime")
+	if lightColorCode2 != "0" {
+		t.Error("Expected", "0", "got", lightColorCode2)
+	}
+
+	lightColorCode3 := getLightColorCode("blue_anime")
+	if lightColorCode3 != "0" {
+		t.Error("Expected", "0", "got", lightColorCode3)
+	}
 }
